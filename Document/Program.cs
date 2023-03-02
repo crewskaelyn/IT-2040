e1248bd43f4889c3dd6eef31a8fb2dbf488ce126
crewskaelyn <116387429+crewskaelyn@users.noreply.github.com>
2023-03-01 19:04:12 -0600

    Add files via upload

---
+++ Document/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+
+namespace Document
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool anotherDoc = true;
+            string file;
+
+            while(anotherDoc) {
+                Console.WriteLine("Document\n");
+                file = fileName();
+
+                fileContents(file);
+
+                Console.WriteLine("\nWould you like to write another file? (y/n)");
+                string answer = Console.ReadLine().ToLower();
+                if (answer != "y") {
+                    anotherDoc = false;
+                }
+            }
+        }
+
+        static string fileName() {
+            string name = "";
+            Console.WriteLine("Enter the name of the document:");
+            try {
+                name = Console.ReadLine();
+            } catch(Exception x) {
+                Console.WriteLine("Exception: " + x.Message);
+            }
+
+            if (name.EndsWith(".txt")) {
+                return name;
+            } else {
+                return name + ".txt";
+            }
+        }
+
+        static void fileContents(string name) {
+            StreamWriter write = File.AppendText(name);
+
+            Console.WriteLine("\nEnter the content for the document:");
+            string content = Console.ReadLine();
+
+            try {
+                write.WriteLine(content);
+            } catch(Exception x) {
+                Console.WriteLine("Exception: " + x.Message);
+            } finally {
+                write.Close();
+                Console.WriteLine($"\n{name} was successfully saved. The document contains {content.Length} characters.");
+            }
+        }
+        
+
+    }
+}
---
+++ FuelGauge/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace FuelGauge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your fuel level in fraction form. Type \"end\" to quit.\n");
+            
+            while (true)
+            {
+                try {
+                    Console.WriteLine("Fraction:");
+                    string input = Console.ReadLine().ToLower();
+
+                    if (input == "end") {
+                        break;
+                    }
+
+                    string[] numbers = input.Split('/');
+                    int x, y;
+                    if (numbers.Length != 2 || !int.TryParse(numbers[0], out x) || !int.TryParse(numbers[1], out y) || y == 0 || x > y) {
+                        continue;
+                    }
+
+                    double percentage = (double)x / y * 100;
+                    if (percentage <= 1) {
+                        Console.WriteLine("E\n");
+                    } else if (percentage >= 99) {
+                        Console.WriteLine("F\n");
+                    } else {
+                        Console.WriteLine(Math.Round(percentage) + "%\n");
+                    }
+
+                } catch (Exception err) {
+                    Console.WriteLine("Error: " + err);
+                }
+            }
+        }
+    }
+}
+
+//https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/types/how-to-convert-a-string-to-a-number
+//https://learn.microsoft.com/en-us/dotnet/api/system.math.round?view=net-7.0#Round2_Example
